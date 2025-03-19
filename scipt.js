7a066d83d0ac80aa8733e0d2c71f072822955c58
Mashrufa-Jasmin-81 <jesminscakegallery81@gmail.com>
2025-03-19 13:20:57 +0600

    Update scipt.js

--- scipt.js
+++ scipt.js
@@ -1,8 +1,3 @@
-// Welcome Popup Close Function
-function closePopup() {
-    document.getElementById('welcome-popup').style.display = 'none';
-}
-
 // Redirect to Order Form with Pre-filled Cake Code
 function orderNow(cakeCode) {
     window.location.href = "order.html?cakeCode=" + cakeCode;
